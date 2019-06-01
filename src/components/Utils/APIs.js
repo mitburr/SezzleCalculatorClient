9df4228eeccdeb361b1fe42c148f8811c84a3f66
Mitchell Burr <mitburr@gmail.com>
2019-06-01 11:25:45 -0500

    api change

--- src/components/Utils/APIs.js
+++ src/components/Utils/APIs.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default{
     addCalculation: function(calculation){
-        return axios.get("http://localhost:4000/create/" + calculation).then( res => {
+        return axios.get("/create/" + calculation).then( res => {
         console.log(res.data);    
          return res.data
         })
