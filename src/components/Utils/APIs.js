c0b1832d49d694bfad7e172a5139b3a1ed19d521
Mitchell Burr <mitburr@gmail.com>
2019-06-01 11:53:16 -0500

    cors proxy

--- src/components/Utils/APIs.js
+++ src/components/Utils/APIs.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default{
     addCalculation: function(calculation){
-        return axios.get("https://glacial-bayou-59948.herokuapp.com/create/" + calculation).then( res => {
+        return axios.get("https://cors-anywhere.herokuapp.com/https://glacial-bayou-59948.herokuapp.com/create/" + calculation).then( res => {
         console.log(res.data);    
          return res.data
         })
