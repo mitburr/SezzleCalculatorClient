8bd55ccb3847fe4b81185a586fd95e01bda77b15
Mitchell Burr <mitburr@gmail.com>
2019-05-31 21:00:34 -0500

    sdofj

---
+++ build/precache-manifest.201448f2bc64b7deea0932b656b1ccd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0937fd05db196a7656a10b9bc7a9dc10",
+    "url": "/SezzleCalculatorClient/index.html"
+  },
+  {
+    "revision": "5e7adffa86d6f15a21c3",
+    "url": "/SezzleCalculatorClient/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f6d147b5eea1c3fb5b51",
+    "url": "/SezzleCalculatorClient/static/js/2.190eadcc.chunk.js"
+  },
+  {
+    "revision": "5e7adffa86d6f15a21c3",
+    "url": "/SezzleCalculatorClient/static/js/main.21685529.chunk.js"
+  },
+  {
+    "revision": "cc63d754a25c0abe3665",
+    "url": "/SezzleCalculatorClient/static/js/runtime~main.cb7e604a.js"
+  }
+]);
